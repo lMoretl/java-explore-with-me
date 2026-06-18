43b17b473162790ce7ae3e25dffd9a8e42539dfc
lMoretl <89445826+lMoretl@users.noreply.github.com>
2026-06-18 21:51:11 +0500

    fix: support stats timestamp format

--- stats/stats-dto/src/main/java/ru/practicum/stats/dto/EndpointHitDto.java
+++ stats/stats-dto/src/main/java/ru/practicum/stats/dto/EndpointHitDto.java
@@ -1,5 +1,6 @@
 package ru.practicum.stats.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,5 +18,7 @@ public class EndpointHitDto {
     private String app;
     private String uri;
     private String ip;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime timestamp;
 }
